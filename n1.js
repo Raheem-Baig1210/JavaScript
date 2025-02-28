96129e6e4a143e6589117782e59dc26e79126d45
Raheem-Baig1210 <raheembaig822@gmail.com>
2025-02-28 08:06:10 +0530

    first commit

---
+++ n1.js
@@ -0,0 +1,58 @@
+// var a=10
+// let b=20
+// const c=30
+
+// console.log(a,b,c)
+
+// var a="Raheem" //string
+// // console.log(typeof(b))
+// var b=20.5 //number 
+// console.log(typeof(b))
+// var a=true
+// console.log(typeof(a))
+
+//control flow statments
+
+
+// var b='10' //string
+// // var c=20 //number
+
+// if(b===10){
+//     console.log("b is equal to 10")
+// }else{
+//     console.log("b is  not equals to 10")
+// }
+
+// var a=15
+// if(a>=18){
+//     console.log("Eligible for voting")
+// } else{
+//     console.log("Not eligible for voting")
+// }
+
+// let marks =55
+// if(marks>=90){
+//     console.log("Grade: A+")
+// }else if(marks >=80){
+//     console.log("Grade: A")
+// }else if(marks>=70){
+//     console.log("Grade: B")
+// }else if(marks >=65){
+//     console.log("Grade: C")
+// }else{
+//     console.log("FAIL")
+// }
+
+
+// var role="user"
+
+// if(role==='admin'){
+//     console.log("You have full access...!!!")
+// }else if(role==="user"){
+//     console.log("you have limited access...!!!!")
+// }else{
+//     console.log("Invalid role, Permission denied ..!!!")
+// }
+
+
+
